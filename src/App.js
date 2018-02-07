22f3f02effa4107b468d8026cb5f749fb34e4b4e
12masta <stanek.marcinp@gmail.com>
2018-02-08 00:06:05 +0100

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/commons'
 
 class App extends Component {
+componentWillMount() {
+  firebase.initializeApp(
+    {
+    apiKey: "AIzaSyCKua3lWs0O581Vx48WNnDaZYpWg7Nsh_0",
+    authDomain: "authentication-c9be8.firebaseapp.com",
+    databaseURL: "https://authentication-c9be8.firebaseio.com",
+    projectId: "authentication-c9be8",
+    storageBucket: "authentication-c9be8.appspot.com",
+    messagingSenderId: "1019348472643"
+  });
+}
+
   render() {
     return (
       <View>
