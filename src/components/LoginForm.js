2306068660f852e04b07a917adc2c184c975e4f0
12masta <stanek.marcinp@gmail.com>
2018-02-08 00:08:24 +0100

    LoginForm init

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+export default LoginForm;
