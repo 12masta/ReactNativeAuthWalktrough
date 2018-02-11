1cb15003b87047dd30acd77d944e6dcb533f9fd9
12masta <stanek.marcinp@gmail.com>
2018-02-11 16:23:12 +0100

    Conditional Rendering of JS

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './commons';
+import { Button, Card, CardSection, Input, Spinner } from './commons';
 
 class LoginForm extends Component {
-state = { email: '', password: '', error: ''};
+state = { email: '', password: '', error: '', loading: false };
 
 onButtonPress() {
   const { email, password } = this.state;
 
-  this.setState({ error: '' });
+  this.setState({ error: '', loading: true });
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .catch(() => {
@@ -18,6 +18,18 @@ onButtonPress() {
             this.setState({error: 'Authentication failed.'});
         });
     });
+}
+
+renderButton() {
+  if(this.state.loading){
+    return <Spinner size="small" />;
+  }
+
+  return (
+    <Button onPress={this.onButtonPress.bind(this)}>
+    Log in
+    </Button>
+  );
 }
 
   render() {
@@ -47,9 +59,7 @@ onButtonPress() {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-          Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
